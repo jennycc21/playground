8a388bac8c19464bb7efe9f858d2cf02fea1e935
jennycc21 <jennywangcc@gmail.com>
2020-07-10 14:00:30 +1000

    Updates

---
+++ precache-manifest.f1e6fb03191acc957a39819198fef83d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c3fe1685ef444d0199a742d3caacce",
+    "url": "/playground/index.html"
+  },
+  {
+    "revision": "2be3dbded2c608504d2e",
+    "url": "/playground/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6702a08bc726b3dfa0d4",
+    "url": "/playground/static/js/2.48f478e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/playground/static/js/2.48f478e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be3dbded2c608504d2e",
+    "url": "/playground/static/js/main.8b382558.chunk.js"
+  },
+  {
+    "revision": "895c97baf350dc475b41",
+    "url": "/playground/static/js/runtime-main.927a30d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/playground/static/media/logo.5d5d9eef.svg"
+  }
+]);
